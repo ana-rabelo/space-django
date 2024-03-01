6100e01f197e4aed114b562efec93eae43de0400
Ana Paula <prabelo79@gmail.com>
2024-03-01 14:10:33 -0300

    change consumed api

--- galeria/views.py
+++ galeria/views.py
@@ -3,27 +3,20 @@ from django.shortcuts import render
 
 def index(request):
 
-    """ info_fotos = {
-    1: {"nome": "Nebulosa de Carina",
-        "legenda": "webbtelescope.org / NASA / James Webb"},
-    2: {"nome": "Galáxia NGC 1079",
-        "legenda": "nasa.org / NASA / Hubble"}   
-    } """
-
-    api_url = "https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/latest_photos"
+    api_url = "https://api.nasa.gov/planetary/apod"
     #esconder a api_key antes de fazer o commit
     api_key = "DEMO_KEY"
-    params = {'api_key': api_key}
+    params = {"api_key": api_key, 
+            "count": 5}
     response = requests.get(api_url, params=params)
 
-    data = response.json()
-    photos = data.get('latest_photos', [])
+    dados = response.json()
         
     info_fotos = {}
-    for i, photo in enumerate(photos, start=1):
+    for i, foto in enumerate(dados, start=1):
         info_fotos[i] = {
-            "nome": f"Foto tirada no sol {photo.get('sol')}",
-            "legenda": photo.get('earth_date', 'Unknown')
+            "nome": foto["title"],
+            "legenda": f"{foto.get('copyright', 'Desconhecido')} / {foto["date"]}"
         }
     
     return render(request, 'galeria/index.html', {"cards": info_fotos})
