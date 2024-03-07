7028c9cfde424efc2b40d43d515633f1fba7c9a0
Ana Paula <prabelo79@gmail.com>
2024-03-07 17:56:32 -0300

    add login form styles and fix footer position

--- usuarios/forms.py
+++ usuarios/forms.py
@@ -4,12 +4,24 @@ class LoginForms(forms.Form):
     nome_login=forms.CharField(
         label='Nome de Usuário', 
         required=True, 
-        max_length=100
+        max_length=100,
+        widget=forms.TextInput(
+            attrs= {
+                'class': 'form-control input__login',
+                'placeholder': 'Digite seu nome de usuário'
+            }
+        )
     )
 
     senha=forms.CharField(
         label='Senha', 
         required=True, 
         max_length=100,
-        widget=forms.PasswordInput()
+        widget=forms.PasswordInput(
+            attrs= {
+                'class': 'form-control input__login',
+                'placeholder': 'Digite sua senha',
+                'type': 'password'
+            }
+        )
     )
