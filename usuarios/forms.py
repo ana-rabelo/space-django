dd218edb2290c3c02cba4ce463cd3d2722a233de
Ana Paula Rabelo <prabelo79@gmail.com>
2024-03-09 10:47:18 -0300

    update sign up form

--- usuarios/forms.py
+++ usuarios/forms.py
@@ -25,3 +25,55 @@ class LoginForms(forms.Form):
             }
         )
     )
+
+class CadastroForms(forms.Form):
+    nome_cadastro=forms.CharField(
+        label='Nome de Usuário', 
+        required=True, 
+        max_length=100,
+        widget=forms.TextInput(
+            attrs= {
+                'class': 'form-control input__login',
+                'placeholder': 'Ex.: João Silva'
+            }
+        )
+    )
+
+    email_cadastro=forms.EmailField(
+        label='E-mail', 
+        required=True, 
+        max_length=100,
+        widget=forms.EmailInput(
+            attrs= {
+                'class': 'form-control input__login',
+                'placeholder': 'Ex.: joaosilva@email.com'
+            }
+        )
+    )
+
+    senha_cadastro=forms.CharField(
+        label='Senha', 
+        required=True, 
+        max_length=70,
+        widget=forms.PasswordInput(
+            attrs= {
+                'class': 'form-control input__login',
+                'placeholder': 'Digite sua senha',
+                'type': 'password'
+            }
+        )
+    )
+
+    senha_cadastro_confirmacao=forms.CharField(
+        label='Confirmação de Senha', 
+        required=True, 
+        max_length=70,
+        widget=forms.PasswordInput(
+            attrs= {
+                'class': 'form-control input__login',
+                'placeholder': 'Digite sua senha novamente',
+                'type': 'password'
+            }
+        )
+    )
+
