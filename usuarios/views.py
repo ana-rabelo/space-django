d2219b158ab4b2718f57f8ceb591f7a0574c7fbe
Ana Paula Rabelo <prabelo79@gmail.com>
2024-03-09 18:02:45 -0300

    add register view

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 
 from usuarios.forms import CadastroForms, LoginForms
+
+from django.contrib.auth.models import User
 
 def login(request):
     form = LoginForms()
@@ -8,4 +10,28 @@ def login(request):
 
 def cadastro(request):
     form = CadastroForms()
+
+    if request.method == 'POST':
+        form = CadastroForms(request.POST)
+      
+        if form.is_valid():
+            if form["senha_cadastro"].value() != form["senha_cadastro_confirmacao"].value():
+                return render(request, 'usuarios/cadastro.html', {'form': form})
+            
+            nome = form["nome_cadastro"].value()
+            email = form["email_cadastro"].value()
+            senha = form["senha_cadastro"].value()
+
+            if User.objects.filter(username=nome).exists():
+                return render(request, 'usuarios/cadastro.html', {'form': form})
+            
+            usuario = User.objects.create_user(
+                username=nome,
+                email=email,
+                password=senha
+            )
+           
+            usuario.save()
+
+            return redirect('login')
     return render(request, 'usuarios/cadastro.html', {'form': form})
