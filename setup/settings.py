d8dc14d1ceef73a12df43a32bb3c0f8b1845f880
Ana Paula Rabelo <50804149+ana-rabelo@users.noreply.github.com>
2024-06-12 10:58:04 -0300

    update settings.py
    Add Render to allowed hosts

--- setup/settings.py
+++ setup/settings.py
@@ -31,8 +31,8 @@ SECRET_KEY = str(os.getenv('SECRET_KEY'))
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['anarabelo.pythonanywhere.com', '127.0.0.1']
-CSRF_TRUSTED_ORIGINS = ['https://anarabelo.pythonanywhere.com']
+ALLOWED_HOSTS = ['spacedjango.onrender.com']
+CSRF_TRUSTED_ORIGINS = ['https://spacedjango.onrender.com']
 
 # Application definition
 
